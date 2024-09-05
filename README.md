eb04e4371ecfbb1d9c434c7da15419d2ca60b1e7
Grapes42 <f.inggrapes@gmail.com>
2024-09-05 22:07:18 +1200

    testing fonts

--- README.md
+++ README.md
@@ -7,6 +7,4 @@
  \____\__, |\___|\__|_| |_|    |/     |/  | |
       |___/                    +------+   |/ 
 ```
-
-
 Cyeth (pronouced like "scythe") is a text based 3D rendering engine made with C++, it is a refined version of my intial project [Pyeth](https://github.com/Grapes42/pyeth), which as the name suggests, was made with Python.
--- code/main.cpp
+++ code/main.cpp
@@ -3,8 +3,18 @@
 #include "graphing.h"
 using namespace std;
 
+void print_arr(int height, int width);
+
 int main() {
     Graphing graphing(10, 10);
+
+    char screen[5][5] = {
+        {' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' '}
+    };
 
     Coord a;
     Coord b;
@@ -13,13 +23,20 @@ int main() {
     a.x = -1;
 
     b.y = 1;
-    a.x = 1;
+    b.x = 1;
 
     vector<Coord> line = graphing.line(a, b);
+
+
 
     /*
     cout << line[0].y << endl
          << line[0].x << endl
          << line[0].z << endl;
     */
+}
+
+
+void print_arr(int height, int width) {
+    
 }
